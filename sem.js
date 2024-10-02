8597e71129fa9815aa326ce71a65f59b3abb8846
kenzanity3 <kennetharante88@gmail.com>
2024-10-03 02:52:44 +0800

    Add files via upload

---
+++ sem.js
@@ -0,0 +1,506 @@
+const data = [
+    {
+            "semlevel": [1, 1],
+            "semsub": [
+            {
+            "course": "NSTP",
+            "description": "Civic Welfare Training Services 1",
+            "unit": 3,
+            "grade": 1.5,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+            {
+            "course": "SCITEC",
+            "description": "Science, Technology and Society",
+            "unit": 3,
+            "grade": 1.6,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+            {
+            "course": "INTCOM",
+            "description": "Introduction to Computing",
+            "unit": 3,
+            "grade": 1.8,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+            {
+            "course": "COPRO1",
+            "description": "Computer Programming 1",
+            "unit": 3,
+            "grade": 2,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+            {
+            "course": "COMPOP",
+            "description": "Computer Operations: Office Productivity Software(MS Office)",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+            {
+            "course": "PEDUC1",
+            "description": "Physical Activities Towards Health and Fitness 1",
+            "unit": 2,
+            "grade": 1.3,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+            {
+            "course": "JRIZAL",
+            "description": "Life, Works and Writings of Jose Rizal",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+            },
+        ]
+    },
+    {
+        "semlevel": [1, 2],
+        "semsub": [
+          {
+            "course": "OOPROG",
+            "description": "Object-Oriented Programming",
+            "unit": 3,
+            "grade": 2.8,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "ARTAPP",
+            "description": "Art Appreciation",
+            "unit": 3,
+            "grade": 1.8,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "PURCOM",
+            "description": "Art Appreciation",
+            "unit": 3,
+            "grade": 1.9,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "NSTP02",
+            "description": "Civic Welfare Training Services 2",
+            "unit": 3,
+            "grade": 1.8,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "DSTRU1",
+            "description": "Discrete Structures 1",
+            "unit": 3,
+            "grade": 2.3,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "PEDUC2",
+            "description": "Physical Activities Towards Health and Fitness 2",
+            "unit": 2,
+            "grade": 1.3,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "COPRO2",
+            "description": "Computer Programming 2",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          },
+          {
+            "course": "GEEL01",
+            "description": "General Education Elective 1",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2022-2023"
+          }
+        ]
+    },
+    {
+        "semlevel": [2, 1],
+       "semsub": [        
+          {
+            "course": "ANIMUL",
+            "description": "Animation and Multimedia",
+            "unit": 3,
+            "grade": 1.9,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          },
+          {
+            "course": "HUMCOM",
+            "description": "Human Computer Interaction",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          },
+          {
+            "course": "MATMOD",
+            "description": "Mathematics in the Modern World",
+            "unit": 3,
+            "grade": 1.6,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          },
+          {
+            "course": "DASTRU",
+            "description": "Computer Programming 1",
+            "unit": 3,
+            "grade": 1.3,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          },
+          {
+            "course": "NETWRK",
+            "description": "CCNA Exploration: Network Fundamentals",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          },
+          {
+            "course": "PEDUC3",
+            "description": "Physical Activities Towards Health and Fitness 3",
+            "unit": 2,
+            "grade": 1.3,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          },
+          {
+            "course": "GEEL02",
+            "description": "General Education Elective 2",
+            "unit": 3,
+            "grade": 1.7,
+            "remark" : "PASSED",
+            "Term": "2023-2024"
+          }
+        ]
+    },
+    {
+        "semlevel": [2, 2],
+        "semsub": [        
+            {
+              "course": "TECHNO",
+              "description": "Technopreneurship",
+              "unit": 3,
+              "grade": 2,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            },
+            {
+              "course": "GEEL3",
+              "description": "General Elective 3",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "UNDSLF",
+              "description": "Understanding the Self",
+              "unit": 3,
+              "grade": 1.5,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            },
+            {
+              "course": "INMANA",
+              "description": "Information Management",
+              "unit": 3,
+              "grade": 1.5,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            },
+            {
+              "course": "EVEDRI",
+              "description": "Event Driven Programming",
+              "unit": 3,
+              "grade": 1.5,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            },
+            {
+              "course": "PROEL1",
+              "description": "Professional Elective 1",
+              "unit": 3,
+              "grade": 1.6,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            },
+            {
+              "course": "PEDUC4",
+              "description": "Physical Activities Towards Health and Fitness 4",
+              "unit": 2,
+              "grade": 1.3,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            }
+        ]
+    },
+    {
+        "semlevel": [3, 1],
+        "semsub": [    
+            {
+              "course": "PROEL2",
+              "description": "Professional Elective 2",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "In progress",
+              "Term": ""
+            },    
+            {
+              "course": "WEBTEC",
+              "description": "Web Technology",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "In progress",
+              "Term": ""
+            },
+            {
+              "course": "MOBAPP_",
+              "description": "Mobile Application",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "In progress",
+              "Term": ""
+            },
+            {
+              "course": "SOFENG",
+              "description": "Software Engineering",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "In progress",
+              "Term": ""
+            },
+            {
+              "course": "RESMET",
+              "description": "Research Methods in IT",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "In progress",
+              "Term": ""
+            },
+            {
+              "course": "SYSTIN",
+              "description": "System Integration and Architecture",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "In progress",
+              "Term": ""
+            }
+        ]
+    },
+    {
+        "semlevel": [3, 2],
+        "semsub": [    
+            {
+              "course": "PROEL3",
+              "description": "Professional Elective 3",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },    
+            {
+              "course": "PROEL4",
+              "description": "Professional Elective 4",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "SOCPRO",
+              "description": "Social and Professional Issues",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "SYSTAD",
+              "description": "System Administration and Maintenance",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "APPDEV",
+              "description": "Application Development and Emerging Technologies",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "CPSTON1",
+              "description": "Capstone Project 1",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            }
+        ]
+    },
+    {
+        "semlevel": [4, 1],
+      "semsub": [    
+            {
+              "course": "INFASS",
+              "description": "Information Assurance and Security",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },    
+            {
+              "course": "OPESYS",
+              "description": "Operating Systems",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "ITPRA1",
+              "description": "IT Practicum 1 (250 Hours)",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "CPSTON2",
+              "description": "Capstone Project 2",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "CONWOR",
+              "description": "The Contemporary World",
+              "unit": 3,
+              "grade": 1.4,
+              "remark" : "PASSED",
+              "Term": "2023-2024"
+            }
+        ]
+    },
+    {
+        "semlevel": [4, 2],
+        "semsub": [    
+            {
+              "course": "PLATEC",
+              "description": "Platform Technologies",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },    
+            {
+              "course": "BUSANA",
+              "description": "Business Analytics",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "ETHICS",
+              "description": "Ethics",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "PHIHIS",
+              "description": "Readings in Philippine History",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            },
+            {
+              "course": "ITPRA2",
+              "description": "IT Practicum 2 (250 Hours)",
+              "unit": 3,
+              "grade": 0,
+              "remark" : "",
+              "Term": ""
+            }
+        ]
+    }
+]
+
+function DecimalPlaces(number, places) {
+    const numberString = number.toString();
+    
+  //check if it have a decimal
+    if (numberString.includes('.')) {
+      const decimalPart = numberString.split('.')[1];
+      if (decimalPart.length < places) {
+        return numberString + '0'.repeat(places - decimalPart.length);
+      } else {
+        return numberString;
+      }
+    } else {
+      return number.toFixed(places);
+    }
+  }
+
+function SemesterTable(data) {
+    const semesterTableElement = document.getElementById("semestertable");
+    const tbody = semesterTableElement.createTBody();
+    
+    data.forEach(semester => {
+      const semlevelRow = tbody.insertRow();
+      semlevelRow.innerHTML = `
+        <td colspan="7" class="table-primary">${semester.semlevel[0] == 1 ? 'First' : (semester.semlevel[0] == 2 ? 'Second' : (semester.semlevel[0] == 3 ? 'Third' : 'Fourth'))} Year, ${semester.semlevel[1] == 1 ? 'First' : 'Second'} Semester</td>
+      `;
+
+      const columnnamerow = tbody.insertRow();
+        columnnamerow.innerHTML = `
+            <th class="table-secondary">Course</th>
+            <th class="table-secondary">Description</th>
+            <th class="table-secondary">Unit</th>
+            <th class="table-secondary">Grade</th>
+            <th class="table-secondary">Remark</th>
+            <th class="table-secondary">Course</th>
+            <th class="table-secondary">Term</th>
+        `;
+
+      semester.semsub.forEach(course => {
+        const courseRow = tbody.insertRow();
+        courseRow.innerHTML = `
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${course.course}</td>
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${course.description}</td>
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${DecimalPlaces(course.unit,1)}</td>
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${course.grade != "0" ? DecimalPlaces(course.grade,2) : ''}</td>
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${course.remark}</td>
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${course.remark != "" ? course.course: ''}</td>
+          <td class = ${course.remark == 'PASSED' ? "table-success" : (course.remark == 'In progress' ? "table-warning" : "table-light")}>${course.Term != '' ? (semester.semlevel[1] == 1 ? 'First' : (semester.semlevel[1] == 2 ? 'Second' : '')) : ''} ${course.Term != '' ? 'Semester A.Y '+course.Term : ''}</td>
+        `;
+      });
+    });
+  }
+  
+  const semesterTableElement = document.getElementById("semestertable");
+  
+  SemesterTable(data);
